5cf283b818dbb201bbb24df897a427f64464a02a
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-05-28 13:21:05 +0300

    new

---
+++ dist/js/script.js
@@ -0,0 +1,34 @@
+//burger
+$('.nav-header__icon').click(function (event) {
+	$(this).toggleClass('active');
+	$('.nav-header__menu').toggleClass('active');
+	if ($(this).hasClass('active')) {
+		$('body').data('scroll', $(window).scrollTop());
+	}
+	$('body').toggleClass('lock');
+	if (!$(this).hasClass('active')) {
+		$('body,html').scrollTop(parseInt($('body').data('scroll')));
+	}
+});
+//=====================================================================================================================
+
+//ibg
+function ibg() {
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+ibg();
+//=====================================================================================================================
+
+// tabs
+$(document).ready(function () {
+	$('ul.news-tabs').on('click', 'li:not(.news-tab__active)', function () {
+		$(this)
+			.addClass('news-tab__active').siblings().removeClass('news-tab__active')
+			.closest('.news-body').find('.news-content').removeClass('news-content__active').eq($(this).index()).addClass('news-content__active');
+	});
+});
+//=====================================================================================================================
